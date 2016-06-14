33061bff26a64fc0a73e3edbfc49830fe7891ef3
Jackson Gonzales <jackson.gonzales@gmail.com>
2016-06-14 13:52:28 -0700

    fix bootstrap, working ajax call in controller

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,9 @@
  * Controller of the ietRestApp
  */
 angular.module('ietRestApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, $http) {
+  $http.get("http://api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=4493568c36ca120429addd929a651947")
+  .then(function(response) {
+    $scope.weatherData = response.data.list;
   });
+});
